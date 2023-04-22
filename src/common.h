38a26178e5b07d096f93705fb358a292204b803b
michaelangel007 <michaelangel007@sharedcraft.com>
2023-04-22 15:45:06 -0700

    Add atoi

--- src/common.h
+++ src/common.h
@@ -2,9 +2,9 @@
     #include <stdio.h>
     #include <string.h> // strlen()
     #include <stdint.h> // uint32_t 
+    #include <stdlib.h> // atoi()
 
 // User
     #include "reverse.h"
     #include "crc32.h"
     #include "compare.h"
-
